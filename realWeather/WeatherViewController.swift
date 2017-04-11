42715679ca70716159e0f618f1feff4aff295f7e
szilardinho <szilardinhog@gmail.com>
2017-04-11 17:13:13 -0400

    Updated UI and implemented protocol methods

--- realWeather/WeatherViewController.swift
+++ realWeather/WeatherViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import Alamofire
 
-class WeatherViewController: UIViewController {
 
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentLocationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
+    
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    //    NSURL()
+       // Alamofire.request(URLRequestConvertible)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        
+    }
+    
+    
+    
 
    
 
